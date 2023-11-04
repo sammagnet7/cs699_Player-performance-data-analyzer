d00ee16288f4166a25f0d4c5e75cedc535aa48a8
Soumik Dutta <sammagnet7@gmail.com>
2023-11-04 15:11:25 +0530

    Adds Cors setting

--- spring-boot-app/playerStatAnalyser/src/main/java/iitb/cs699/playerStatAnalyser/PlayerStatAnalyserApplication.java
+++ spring-boot-app/playerStatAnalyser/src/main/java/iitb/cs699/playerStatAnalyser/PlayerStatAnalyserApplication.java
@@ -2,6 +2,9 @@ package iitb.cs699.playerStatAnalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PlayerStatAnalyserApplication {
@@ -9,5 +12,16 @@ public class PlayerStatAnalyserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlayerStatAnalyserApplication.class, args);
 	}
+	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer()
+	    {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedOrigins("*");
+	            }
+	        };
+	    }
 
 }
